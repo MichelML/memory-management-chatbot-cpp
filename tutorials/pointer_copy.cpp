6648c2574470ca81709e7c9cdb81c979dedc107c
michelml <michmoreau.l@gmail.com>
2021-04-22 19:15:40 -0400

    format

--- tutorials/pointer_copy.cpp
+++ tutorials/pointer_copy.cpp
@@ -1,34 +1,30 @@
 #include <iostream>
 
-class MyClass
-{
+class MyClass {
 private:
-    int *_myInt;
+  int *_myInt;
 
 public:
-    MyClass()
-    {
-        _myInt = (int *)malloc(sizeof(int));
-    };
-    ~MyClass()
-    {
-        free(_myInt);
-    };
-    void printOwnAddress() { std::cout << "Own address on the stack is " << this << std::endl; }
-    void printMemberAddress() { std::cout << "Managing memory block on the heap at " << _myInt << std::endl; }
+  MyClass() { _myInt = (int *)malloc(sizeof(int)); };
+  ~MyClass() { free(_myInt); };
+  void printOwnAddress() {
+    std::cout << "Own address on the stack is " << this << std::endl;
+  }
+  void printMemberAddress() {
+    std::cout << "Managing memory block on the heap at " << _myInt << std::endl;
+  }
 };
 
-int main()
-{
-    // instantiate object 1
-    MyClass myClass1;
-    myClass1.printOwnAddress();
-    myClass1.printMemberAddress();
+int main() {
+  // instantiate object 1
+  MyClass myClass1;
+  myClass1.printOwnAddress();
+  myClass1.printMemberAddress();
 
-    // copy object 1 into object 2
-    MyClass myClass2(myClass1); // copy constructor
-    myClass2.printOwnAddress();
-    myClass2.printMemberAddress();
+  // copy object 1 into object 2
+  MyClass myClass2(myClass1); // copy constructor
+  myClass2.printOwnAddress();
+  myClass2.printMemberAddress();
 
-    return 0;
+  return 0;
 }
