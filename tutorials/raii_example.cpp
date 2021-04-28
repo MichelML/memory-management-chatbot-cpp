265d4157914b759e2fb3cb1bede083795c60ac28
michelml <michmoreau.l@gmail.com>
2021-04-28 10:36:42 -0400

    format

--- tutorials/raii_example.cpp
+++ tutorials/raii_example.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 
-class MyInt
-{
-    int *_p; // pointer to heap data
+class MyInt {
+  int *_p; // pointer to heap data
 public:
-    MyInt(int *p = NULL) { _p = p; }
-    ~MyInt() 
-    { 
-        std::cout << "resource " << *_p << " deallocated" << std::endl;
-        delete _p; 
-    }
-    int &operator*() { return *_p; } // // overload dereferencing operator
+  MyInt(int *p = NULL) { _p = p; }
+  ~MyInt() {
+    std::cout << "resource " << *_p << " deallocated" << std::endl;
+    delete _p;
+  }
+  int &operator*() { return *_p; } // // overload dereferencing operator
 };
 
 int main() {
@@ -26,15 +24,13 @@ int main() {
     delete en;
   }
 
-    for (size_t I = 0; I < 5; ++I)
-    {
-        // allocate the resource on the stack
-        MyInt en(new int(I));
+  for (size_t I = 0; I < 5; ++I) {
+    // allocate the resource on the stack
+    MyInt en(new int(I));
 
-        // use the resource
-        std::cout << *en << "/" << den[I] << " = " << *en / den[I] << std::endl;
-    }
-
+    // use the resource
+    std::cout << *en << "/" << den[I] << " = " << *en / den[I] << std::endl;
+  }
 
   return 0;
 }
